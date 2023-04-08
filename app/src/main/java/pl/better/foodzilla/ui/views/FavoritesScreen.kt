99a56e3d9e7c7fafe37cd5adcb04ade87bbf786c
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-04-08 19:15:55 +0200

    Tab layout in favorites screen

--- app/src/main/java/pl/better/foodzilla/ui/views/FavoritesScreen.kt
+++ app/src/main/java/pl/better/foodzilla/ui/views/FavoritesScreen.kt
@@ -1,12 +1,23 @@
 package pl.better.foodzilla.ui.views
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.launch
 import pl.better.foodzilla.data.models.login.Login
 import pl.better.foodzilla.ui.navigation.BottomBarNavGraph
 
+@OptIn(ExperimentalFoundationApi::class)
 @BottomBarNavGraph
 @Destination
 @Composable
@@ -14,5 +25,38 @@ fun FavoritesScreen(
     navigator: DestinationsNavigator,
     user: Login?
 ) {
-    Text(text = "FAVORITES SCREEN")
+    val tabRowItems = listOf(
+        "Favorite recipes",
+        "Favorite searches"
+    )
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
+    Column(modifier = Modifier.fillMaxSize()) {
+        TabRow(
+            backgroundColor = Color.White,
+            contentColor = Color.Black,
+            selectedTabIndex = pagerState.currentPage,
+        ) {
+            tabRowItems.forEachIndexed { index, item ->
+                Tab(
+                    text = { Text(item) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                )
+            }
+        }
+        HorizontalPager(
+            verticalAlignment = Alignment.CenterVertically,
+            pageCount = tabRowItems.size,
+            state = pagerState
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Text(text = tabRowItems[it])
+            }
+        }
+    }
 }
