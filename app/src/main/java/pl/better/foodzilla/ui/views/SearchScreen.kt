a7eef148dea8e2ee69d393954f761e41e07da81f
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-04-26 15:54:38 +0200

    Ingredients search

--- app/src/main/java/pl/better/foodzilla/ui/views/SearchScreen.kt
+++ app/src/main/java/pl/better/foodzilla/ui/views/SearchScreen.kt
@@ -1,5 +1,6 @@
 package pl.better.foodzilla.ui.views
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -8,6 +9,7 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import pl.better.foodzilla.ui.components.ButtonRoundedCorners
 import pl.better.foodzilla.ui.navigation.BottomBarNavGraph
 import pl.better.foodzilla.ui.viewmodels.SearchScreenViewModel
+import pl.better.foodzilla.ui.views.destinations.IngredientsSearchScreenDestination
 import pl.better.foodzilla.ui.views.destinations.TagsSearchScreenDestination
 
 @BottomBarNavGraph
@@ -17,7 +19,12 @@ fun SearchScreen(
     navigator: DestinationsNavigator,
     viewModel: SearchScreenViewModel = hiltViewModel(),
 ) {
-    ButtonRoundedCorners(buttonText = "Tags", textColor = Color.White) {
-        navigator.navigate(TagsSearchScreenDestination())
+    Column {
+        ButtonRoundedCorners(buttonText = "Tags", textColor = Color.White) {
+            navigator.navigate(TagsSearchScreenDestination())
+        }
+        ButtonRoundedCorners(buttonText = "Ingredients", textColor = Color.White) {
+            navigator.navigate(IngredientsSearchScreenDestination())
+        }
     }
 }
